b2aa2570da8df835dadc0a03eb727bc9ed69bf20
Vagabundo <robertodelrior@gmail.com>
2024-03-10 15:53:38 +0100

    Update postman test battery

--- src/MinimalTranslator.Core/MinimalTranslator.Application/Services/AzureHttpTextAnalyticService.cs
+++ src/MinimalTranslator.Core/MinimalTranslator.Application/Services/AzureHttpTextAnalyticService.cs
@@ -1,8 +1,8 @@
-using System.Text;
-using Newtonsoft.Json;
-using MinimalTranslator.Application.Interfaces;
-using System.Net.Http.Json;
 using MinimalTranslator.Application.Data.Azure;
+using MinimalTranslator.Application.Interfaces;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Text;
 
 namespace MinimalTranslator.Application.Services;
 
